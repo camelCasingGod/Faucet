9abfe779e7fdec97cdcf7b00357ff0a8094e8d5b
Ameya Phadnis <aphadnis@berkeley.edu>
2022-06-09 20:48:20 -0700

    Used Ganache

--- truffle-config.js
+++ truffle-config.js
@@ -37,13 +37,14 @@ module.exports = {
   networks: {
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
+     port: 7545     ,       // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     }
   },
   compilers: {
     solc: {
-      version: "0.5.1",      // Fetch exact version from solc-bin (default: truffle's version)
+      // downgraded mine for debugging
+      version: "0.8.14",      // Fetch exact version from solc-bin (default: truffle's version)
     }
   }
 };
